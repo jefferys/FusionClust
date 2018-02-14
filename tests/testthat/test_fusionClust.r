90e00889f94a98663d48d2bf89f153c39ddd625e
Stuart R. Jefferys <srj@unc.edu>
2018-02-14 18:24:39 +0000

    Modified readAbraSv to load tumor only samples.
    Added minimal testing and some updated documentation. Check fails due to rmd document. Also added some corrections to the DESCRIPTION file (trailing space not allowed in version) and changed the name of the test file for the single-case to distinguish from multi sample tumor only.

--- tests/testthat/test_fusionClust.r
+++ tests/testthat/test_fusionClust.r
@@ -1,33 +1,38 @@
 context( "Testing functions in file fusionClust.R" )
 
-svSampleFile <- "data/perSampleAbraSv.tsv"
+svSampleFile <- "data/abraSv.tsv"
 svSampleData <- readLines( svSampleFile )
+svMultiTumorOnlyFile <- "data/abraSvMultiTumorOnly.tsv"
+svMultiTumorOnlyData <- readLines( svMultiTumorOnlyFile )
 
 describe( "Test dependencies infrastructure", {
   it( "includes a file of abra-derived structural variants.", {
     expect_true( file.exists( svSampleFile ))
     expect_true( length(svSampleData) > 0)
   })
-})
-
-describe( "Testing loadAbraSv() ...", {
-  describe( "with default parameters ..." , {
-    describe( "with file = single file", {
-      expect_silent( fusions <- loadAbraSv(svSampleFile) )
-      expect_is( fusions, "data.frame" )
-      expect_equal( nrow(fusions), length(svSampleData) )
-    })
+  it( "includes a file of multi-sample, tumor only abra-derived structural variants.", {
+    expect_true( file.exists( svMultiTumorOnlyFile ))
+    expect_true( length(svMultiTumorOnlyData) > 0)
   })
 })
 
-describe( "Testing orderBy() ...", {
-  describe( "with default parameters ...", {
-    describe( "map= is character vector map.", {
-      it( "Orders correctly", {
-        got <- orderBy( c("A", "B", "C"), c("C", "B", "A") )
-        want <- c(3,2,1)
-        expect_equal(got, want)
-      })
-    })
-  })
+describe( "Testing readAbraSv() ...", {
+	describe( "with default parameters ..." , {
+		describe( "with file = single file", {
+			it ( "Reads the correct number of rows, into a data frame, without error", {
+				expect_silent( fusions <- readAbraSv(svSampleFile) )
+				expect_is( fusions, "data.frame" )
+				expect_equal( nrow(fusions), length(svSampleData) )
+			})
+		})
+	})
+	describe( "tumorOnly= parameter effects", {
+		describe( "with multiple samples ...", {
+			it ( "Reads the correct number of rows, into a data frame, without error", {
+				expect_silent( fusions <- readAbraSv(svMultiTumorOnlyFile, tumorOnly= TRUE) )
+				expect_is( fusions, "data.frame" )
+				expect_equal( nrow(fusions), length(svMultiTumorOnlyData) )
+			})
+		})
+	})
 })
